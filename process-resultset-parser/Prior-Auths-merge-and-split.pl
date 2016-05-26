5258825e5a39f9998c84b2f9ddb6f132b597a301
Inigo San Gil <isangil@lternet.edu>
2016-05-26 10:31:36 -0600

    Breakes  down process for readability

--- process-resultset-parser/Prior-Auths-merge-and-split.pl
+++ process-resultset-parser/Prior-Auths-merge-and-split.pl
@@ -7,10 +7,10 @@
 # For each file (mo/ro; chemo; medicare [ x2] ) :
 #
 #  -- open master, complete, original.
-#      -scan master : completed to completes, others stay.
+#     -scan master : completed to completes, others stay.
 #     -open daily raw
-#       -write out original, [or leave it as is in FS]
-#       -parse daily raw. Append to master, adding, minding cols and formatting.
+#     -write out original, [or leave it as is in FS]
+#     -parse daily raw. Append to master, adding, minding cols and formatting.
 #
 #  * originals as come out of SQL job
 #
@@ -28,19 +28,32 @@ my @master_files = ('MO_and_RO_WeekOut_PriorAuths.csv',
              
 #my @original_files = ('SQL_MO_and_RO_WeekOut_PriorAuths.csv', 'SQL_MO_and_RO_AddOn_PriorAuths.csv',  'SQL_CHEMO_WeekOut_PriorAuths.csv',  'SQL_CHEMO_AddOn_PriorAuths.csv',  'SQL_MEDICARE_WeekOut_PriorAuths.csv',    'SQL_MEDICARE_AddOn_PriorAuths.csv');
 
-# my ($cfn, $sfn);                                      # filenames
-
 my $m_a; my $s_a;  my $c_a ;                 # contents of file arrays
 my ($m_fh, $c_fh , $s_fh);                        # filehandles
 
 foreach my $m_fn (@master_files){
+   
      my $s_fn = 'SQL_' . $m_fn;
-     my $c_fn = 'COMPLETE_' . $m_fn;
+
+
+     ##  extract completes and pendings from master
+     (my @completes, my @pendings) = get_completes_and_pendings($m_fn);
      
-     ## extract completes from master.
-     look_up_and_dump_completes($m_fn);
+     ## add completes to existing file
+     add_completed(\@completes, $m_fn);
      
-     ##
+     ## erase master
+     erase_master($m_fn);
+     
+     ## new master with pending records
+     pendings_to_master(\@pendings, $m_fn);
+     
+     ## get new additions
+     my @records = read_new($m_fn);
+     
+     ## append records
+     append_records(\@records, $m_fn);
+     
 }
 
 sub slurp
@@ -54,7 +67,7 @@ sub slurp
     return $contents;
 }
 
-sub look_up_and_dump_completes
+sub get_completes_and_pendings
 {
    use Text::CSV_XS;
   ## use List::MoreUtils;
@@ -77,35 +90,60 @@ sub look_up_and_dump_completes
       }
    }
    close $iom or die "no close for $master_filename: $!";
+
+   return (\@completed_rows, \@pending_rows);
    
-   ## create a hash of statuses.
-   ##my @x = indexes { $_ =~/COMPLETED/ } @column;
-     
-   # open the completes. create csv object, then add to existing file.
+}
+ 
+sub add_completed
+{
+   my @completes = @{$_[0]};
+   my $master_filename = $_[1];
+   my $complete_fn = 'COMPLETE_' . $m_fn;    
+  # Create csv object
    my $complete_csv = Text::CSV_XS->new ({ binary => 1, eol => $/ });
+   
+  # Open the completes. 
    my $complete_fn = 'COMPLETE_' . $master_filename;
    open my $complete_fh, ">>", $complete_fn or die "$complete_fh: $!"; 
-   $complete_csv->say ($complete_fh, $_) for @completed_rows;
-   close $complete_fh or die "could not close file $complete_csv: $!";  
+   # Add completes to existing file.
+   $complete_csv->say ($complete_fh, $_) for @completes;
+   close $complete_fh or die "could not close file $complete_csv: $!";  \
    
-   ## open daily SQL raw csv, add to trimmed down master
+   return;
    
-    open my $iom, "<", $master_filename or die "cannot open $master_filename: $!";
+}   
 
-   ##get only the 7th column 
-   ##my @column = map { $_->[6] } @{$csv->getline_all ($iom)};
-   my @completed_rows; my @pending_rows;
-   while (my $master_row = $csv->getline ($iom)) {
-      if( $master_row->[6] =~ m/COMPLETED/ ){  # 7th field should match
-                                                                               ### HERE, get me the statuses.
-         push @completed_rows, $master_row;
-         ## get me the person who completed, add to counter.  HERE
-      }else{
-         push @pending_rows, $master_row;
-      }
-   }
-   close $iom or die "no close for $master_filename: $!";
+sub  erase_master
+{
+    my $fname = shift;
+    unlink $fname or warn "Could not erase $file: $!";
+    return;
     
 }
+         
+sub pendings_to_master
+{
+   my @rows = @{$_[0]};
+   my $master_filename = $_[1];
+     ## needs work
+  return;
      
+}
+
+sub  read_new
+{
+   my $master_filename = shift;
+     ##news work slurp call?
+     my @newrows;
      
+     return (\@newrows);
+}
+     
+sub append_records
+{
+   my @recs = @{$_[0]};
+   my $master_filename = $_[1];
+   #needs work
+   return;
+}
