5e8f0ac16caab8ab603f520c5a716e0a108e65b7
Inigo San Gil <isangil@lternet.edu>
2016-06-02 15:54:38 -0600

    Many fixes, this version passed mustard

--- process-resultset-parser/Prior-Auths-merge-and-split.pl
+++ process-resultset-parser/Prior-Auths-merge-and-split.pl
@@ -1,12 +1,11 @@
 #
-#  Perl script Prior-Auths-merge-and-split    Inigo San Gil, May 2016
+#  Perl script Prior-Auths-merge-and-split    Inigo San Gil, June 2016
 #  
 #  Requisites and dependencies
 #     Needs Perl, and CPAN extension Text::CSV_XS
 #     The six master files
 #     The six new csv files from queries
 #     The six completed files to log
-#     Optionally, a log / reporting file
 #
 #  Description:
 # 
@@ -30,18 +29,13 @@ my @master_files = ('MO_and_RO_WeekOut_PriorAuths.csv',
                                'MEDICARE_WeekOut_PriorAuths.csv',
                                'MEDICARE_AddOn_PriorAuths.csv');
                                
-#my @completed_files = ('COMPLETE_MO_and_RO_WeekOut_PriorAuths.csv',   'COMPLETE_MO_and_RO_AddOn_PriorAuths.csv',    'COMPLETE_CHEMO_WeekOut_PriorAuths.csv', 'COMPLETE_CHEMO_AddOn_PriorAuths.csv',    'COMPLETE_MEDICARE_WeekOut_PriorAuths.csv', 'COMPLETE_MEDICARE_AddOn_PriorAuths.csv');
-             
-#my @original_files = ('SQL_MO_and_RO_WeekOut_PriorAuths.csv', 'SQL_MO_and_RO_AddOn_PriorAuths.csv',  'SQL_CHEMO_WeekOut_PriorAuths.csv',  'SQL_CHEMO_AddOn_PriorAuths.csv',  'SQL_MEDICARE_WeekOut_PriorAuths.csv',    'SQL_MEDICARE_AddOn_PriorAuths.csv');
-
-my $m_a; my $s_a;  my $c_a ;                 # contents of file arrays
-my ($m_fh, $c_fh , $s_fh);                        # filehandles
-
 foreach my $m_fn (@master_files){
    
      ##  extract completes and pendings from master
-     (my @completes, my @pendings) = get_completes_and_pendings($m_fn);
+     my @completes = get_completes($m_fn);
      
+     my @pendings = get_pendings($m_fn);
+          
      ## add completes to existing file
      add_completed(\@completes, $m_fn);
      
@@ -56,64 +50,71 @@ foreach my $m_fn (@master_files){
      
      ## append records
      append_records(\@records, $m_fn);
-     
-     ## reporting
-     ##
+          
 }
 
-sub slurp
+sub get_completes
 {
-    my $filename = shift;
-    open my $in, '<', $filename
-        or die "Cannot open '$filename' for slurping - $!";
-    local $/;
-    my $contents = <$in>;
-    close($in);
-    return $contents;
-}
+#  use POSIX qw(strftime);
+ #  my $today = strftime "%m/%d/%Y", localtime;
+   my $master_filename = shift;   
+  
+   open my $iom, '<', $master_filename or die "cannot open $master_filename: $!";
 
-sub get_completes_and_pendings
-{
-   use Text::CSV_XS;
-  ## use List::MoreUtils;
+   my @rows;
    
-   my $master_filename = shift;
-   my $csv = Text::CSV_XS->new ({ binary => 1 });
-
-   open my $iom, "<", $master_filename or die "cannot open $master_filename: $!";
-
-   ##get only the 7th column 
-   ##my @column = map { $_->[6] } @{$csv->getline_all ($iom)};
-   my @completed_rows; my @pending_rows;
-   while (my $master_row = $csv->getline ($iom)) {
-      if( $master_row->[6] =~ m/COMPLETED/ ){  # 7th field should match
-                                                                               ### HERE, get me the statuses.
-         push @completed_rows, $master_row;
-         ## get me the person who completed, add to counter.  HERE
-      }else{
-         push @pending_rows, $master_row;
+   while (my $master_row = <$iom>) {
+      
+      my @mrow = split(/,/, $master_row);
+      
+      if ( $mrow[9] =~ /COMPLETE|DENIED|NO PA(.*)NEEDED|APPEAL/i){
+           push @rows, $master_row;
       }
    }
    close $iom or die "no close for $master_filename: $!";
 
-   return (\@completed_rows, \@pending_rows);
+   return (@rows);
    
 }
- 
+
+sub get_pendings
+{
+   my $master_filename = shift;
+   
+   open my $iom, '<', $master_filename or die "cannot open $master_filename: $!";
+
+   my @rows;
+   
+   while (my $master_row = <$iom>) {
+      
+      my @mrow = split(/,/, $master_row);
+      print "get_pendings: INS $mrow[7] \n"; 
+      my $var =  $mrow[9] ; # was 7
+      if ( $var !~ /COMPLETE|DENIED|NO PA NEEDED|NO PA\/REF NEEDED|APPEAL/i ) {   #
+      ##    print "get_pendings: Pending Row  $mrow[7] \n";
+          push @rows, $master_row;
+      }
+      
+   }
+   close $iom or die "no close for $master_filename: $!";
+
+   return (@rows);
+   
+}
+
 sub add_completed
 {
    my @completes = @{$_[0]};
    my $master_filename = $_[1];
-   my $complete_fn = 'COMPLETE_' . $master_filename;    
-  # Create csv object
-   my $complete_csv = Text::CSV_XS->new ({ binary => 1, eol => $/ });
-   
+   my $complete_fn = 'COMPLETE_' . $master_filename;
   # Open the completes. 
    open my $complete_fh, ">>", $complete_fn or die "$complete_fn: $!"; 
-   # Add completes to existing file.
-   $complete_csv->say ($complete_fh, $_) for @completes;
-   close $complete_fh or die "add_completed: Could not close file $complete_fn: $!";  \
-   
+   foreach my $el (@completes)
+   {
+   ##   print "add_completed: Element Row: $el";
+    print $complete_fh "$el"; # Print each entry in our array to the file
+   }
+   close $complete_fh or die "add_completed: Could not close file $complete_fn: $!";
    return;
    
 }   
@@ -128,16 +129,18 @@ sub  erase_master
          
 sub pendings_to_master
 {
+     
    my @rows = @{$_[0]};
    my $master_filename = $_[1];
-   # Create csv object
-   my $work_csv = Text::CSV_XS->new ({ binary => 1, eol => $/ });
    
    # Open the new master filename
    open my $master_fh, ">>", $master_filename or die "$master_filename: $!"; 
-   # Add pendings to master
-   $work_csv->say ($master_fh, $_) for @rows;
-   close $master_fh or die "pendinds_to_master: Could not close file $master_filename: $!";  \
+
+   foreach my $el (@rows)
+   {
+     print $master_fh "$el"; # Print each entry in our array to the file
+   }
+   close $master_fh or die "pendings_to_master: Could not close file $master_filename: $!";  \
    
    return;
      
@@ -145,39 +148,38 @@ sub pendings_to_master
 
 sub  read_new
 {
+ 
    my $master_filename = shift;
-   my $s_fn = 'SQL_' . $master_filename;
-   
-   my $csv = Text::CSV_XS->new ({ binary => 1 });
-   open my $ion, "<", $s_fn or die "read_new: Cannot open $s_fn : $!";
+   my $s_fn = 'SQL_' . $master_filename;   
+
+   open my $ion, '<', $s_fn or die "read_new: Cannot open $s_fn : $!";
 
    my @newrows; 
-   while (my $row = $csv->getline ($ion)) {
       
+   while (my $row = <$ion>) {
       push @newrows, $row;
-      
    }
+   
    close $ion or die "no close for $s_fn: $!";
      
-   return (\@newrows);
+   return (@newrows);
 }
      
 sub append_records
 {
    my @newrows = @{$_[0]};  
    my $master_filename = $_[1];
-    
-   # Create csv object
-   my $master_csv = Text::CSV_XS->new ({ binary => 1, eol => $/ });
-  # Open the master. 
-   my $master_fn = $master_filename;
-   open my $master_fh, ">>", $master_fn or die "$master_fn: $!"; 
+
+   # Open the master. 
+   open my $master_fh, ">>", $master_filename or die "$master_filename: $!"; 
    
-   ##may be i need to add-take to newrows here.
-   ##
-   ##  
    # Add completes to existing file.
-   $master_csv->say ($master_fh, $_) for @newrows;
+   foreach my $el (@newrows)
+   {
+     #print "append_record: Row is $el";
+     print $master_fh "$el"; # Print each entry in our array to the file
+   }
    close $master_fh or die "append_records: Could not close file $master_filename: $!";  \
    return;
 }
+
